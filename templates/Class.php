31059b72736478dfd0b6f4ffd0f6a8dcf2843fce
sevidmusic <sevidmusic@gmail.com>
2023-08-01 11:08:47 -0400

    Resolved issue #15

--- templates/Class.php
+++ templates/Class.php
@@ -2,7 +2,7 @@
 
 namespace __ROOT_NAMESPACE__\classes\__SUB_NAMESPACE__;
 
-use __ROOT_NAMESPACE__\interfaces\__SUB_NAMESPACE__\__TARGET_CLASS_NAME__ as __TARGET_CLASS_NAME__Interface;
+use \__ROOT_NAMESPACE__\interfaces\__SUB_NAMESPACE__\__TARGET_CLASS_NAME__ as __TARGET_CLASS_NAME__Interface;
 
 class __TARGET_CLASS_NAME__ implements __TARGET_CLASS_NAME__Interface
 {
